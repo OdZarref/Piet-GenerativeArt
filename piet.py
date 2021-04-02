2f5ab0fb259adaf0e7f6f6e57c2cc90e01b8ff8f
Od Zarref <ludwigvonmarcelo@gmail.com>
2021-04-02 13:11:53 -0300

    Refatorando código

--- piet.py
+++ piet.py
@@ -2,26 +2,24 @@ from PIL import Image
 from random import randint, choice
 
 class Piet:
-    def __init__(self):
-        self.image = Image.new('RGB', (1580, 1580), color = (1, 1, 1))
-        self.colors = [(0, 0, 0,), (255, 255, 255), (255, 0, 0), (0, 255, 0), (0, 0, 255), (255,0,255), (75,0,130), (255,127,80), (0,255,255), (47,79,79), (128,128,0)]
+    def createImageSquared(self):
+        from config import colors
 
-    def createImage(self):
+        image = Image.new('RGB', (1580, 1580), color = (1, 1, 1))
         placeHorizontal = 0
-        placeVertical = 0 
+        placeVertical = 0
         shapeSize = (300, 300)
 
         for _ in range(5):
             for _ in range(5):
-                shapeToPaste = Image.new('RGB', (shapeSize[0], shapeSize[1]), color = (choice(self.colors)))
-                self.image.paste(shapeToPaste, (placeHorizontal, placeVertical))
+                shapeToPaste = Image.new('RGB', (shapeSize[0], shapeSize[1]), color = (choice(colors)))
+                image.paste(shapeToPaste, (placeHorizontal, placeVertical))
                 placeVertical = placeVertical + 300 + 20
-                self.image.save('image.jpg')
-            
+                image.save('image.jpg')
+
             placeHorizontal = placeHorizontal + 300 + 20
             placeVertical = 0
 
 if __name__ == '__main__':
     piet = Piet()
-    piet.createImage()
-
+    piet.createImage())
