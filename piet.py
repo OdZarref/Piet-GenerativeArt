298f0d84cc2d68d10ac448072a7682d18ec2e785
Od Zarref <ludwigvonmarcelo@gmail.com>
2021-04-05 17:50:37 -0300

    Refatorando código

--- piet.py
+++ piet.py
@@ -9,48 +9,61 @@ from config import *
 
 class Piet:
     def createImage(self):
-        def split(self, edge, width, height):
+        def split(self, activeFrame):
             splitOnVertical = choice((True, False))
+            position = activeFrame['pos']
+            width = activeFrame['width']
+            height = activeFrame['height']
 
             if splitOnVertical:
-                whereToSplit = randint(0, width-50)
-                newWidth = width - whereToSplit
-                newWidth2 = width - newWidth
-                whereToDraw = edge[:]
-                whereToDraw2 = (newWidth, edge[-1])
-                print(f'figura 1: Vertical = Sim, Comprimento = {newWidth}x{height}, Posição = {whereToDraw}')
-                print(f'figura 2: Vertical = Sim, Comprimento = {newWidth2}x{height}, Posição = {whereToDraw2}')
-                draw(self, whereToDraw, newWidth, height)
-                draw(self, whereToDraw2, newWidth2, height)
-                if not whereToDraw in edgers: edgers.append(whereToDraw)
-                if not whereToDraw2 in edgers: edgers.append(whereToDraw2)
+                whereToSplit = randint(200 , width)
+                widthObject1 = width - whereToSplit
+                widthObject2 = width - widthObject1
+                object1 = {'pos': position[:], 'width': widthObject1, 'height': height, 'ignore': False}
+                object2 = {'pos': (widthObject1, position[-1]), 'width': widthObject2, 'height': height, 'ignore': False}
+                print(f'figura 1: Vertical = Sim, Comprimento = {widthObject1}x{height}, Posição = {object1["pos"]}')
+                print(f'figura 2: Vertical = Sim, Comprimento = {widthObject2}x{height}, Posição = {object2["pos"]}')
+                draw(self, object1['pos'], widthObject1, height)
             else:
-                whereToSplit = randint(0, height-50)
-                newHeight = height - whereToSplit
-                newHeight2 = height - newHeight
-                whereToDraw = edge[:]
-                whereToDraw2 = (edge[0], newHeight)
-                print(f'figura 1: Vertical = Não, Comprimento = {width}x{newHeight}, Posição = {whereToDraw}')
-                print(f'figura 2: Vertical = Não, Comprimento = {width}x{newHeight2}, Posição = {whereToDraw2}')
-                draw(self, whereToDraw, width, newHeight)
-                draw(self, whereToDraw2, width, newHeight2)
-                if not whereToDraw in edgers: edgers.append(whereToDraw)
-                if not whereToDraw2 in edgers: edgers.append(whereToDraw2)
+                whereToSplit = randint(200, height)
+                heightObject1 = height - whereToSplit
+                heightObject2 = height - heightObject1
+                object1 = {'pos': position[:], 'width': width, 'height': heightObject1, 'ignore': False}
+                object2 = {'pos': (position[0], heightObject1), 'width': width, 'height': heightObject2, 'ignore': False}
+                print(f'figura 1: Vertical = Não, Comprimento = {width}x{heightObject1}, Posição = {object1["pos"]}')
+                print(f'figura 2: Vertical = Não, Comprimento = {width}x{heightObject2}, Posição = {object2["pos"]}')
+                draw(self, object1['pos'], width, heightObject1)
 
-        def draw(self, whereToDraw, width, height):
+            if not object1 in frames:
+                if object1['width'] < 300 or object1['height'] < 300: object1['ignore'] = True
+                frames.append(object1)
+
+            if not object2 in frames:
+                if object1['width'] < 300 or object1['height'] < 300: object1['ignore'] = True
+                frames.append(object2)
+
+            activeFrame['ignore'] = True
+
+        def draw(self, pos, width, height):
             colorToPaint = (randint(0, 255), randint(0, 255), randint(0, 255))
             imageToPaste = Image.new('RGB', (width, height), color = colorToPaint)
-            image.paste(imageToPaste, whereToDraw)
-            image.save('image.jpg')
+            image.paste(imageToPaste, pos)
+            image.save(f'image.jpg')
 
-        width = 1500
-        height = 1500
-        edgers = [(0, 0)]
-        image = Image.new('RGB', (width, height), color = (1, 1, 1))
+        frames = [{'pos': (0, 0), 'width': 1500, 'height': 1500, 'ignore': False}]
+        image = Image.new('RGB', (frames[0]['width'], frames[0]['height']), color = (randint(0, 255), randint(0, 255), randint(0, 255)))
 
-        for index in range(randint(5, 8)):
-            split(self, choice(edgers), width, height)
-            print(edgers)
+        for counter in range(5):
+            possibleFrames = list()
+
+            for frame in frames:
+                if not frame['ignore']: activeFrame = frame
+
+            # activeFrame = choice(possibleFrames)
+
+            for frame in frames: print(frame)
+            print(f'Objeto Ativo: {activeFrame}')
+            split(self, activeFrame)
             input()
 
 class Twitter:
@@ -77,4 +90,6 @@ if __name__ == '__main__':
     #     schedule.run_pending()
     #     sleep(2)
 
-    runScript()
+    while True:
+        runScript()
+        sleep(1)
